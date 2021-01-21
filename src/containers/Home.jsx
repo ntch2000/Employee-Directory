43bce5d597a5029da17f3e3b2fdd9e8d9ba61375
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2021-01-20 23:09:15 -0500

    Added API code and hard coded state values. Hard coded data displays in app

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../utils/API";
+import moment from "moment";
 
 const Home = () => {
+  // set state
+  const [employees, setEmployees] = useState([
+    {
+      gender: "female",
+      name: {
+        title: "Mrs",
+        first: "Alice",
+        last: "Miller",
+      },
+      location: {
+        street: {
+          number: 9812,
+          name: "St. Catherine St",
+        },
+        city: "Belmont",
+        state: "Nova Scotia",
+        country: "Canada",
+        postcode: "A5R 4J5",
+        coordinates: {
+          latitude: "-59.9869",
+          longitude: "99.0660",
+        },
+        timezone: {
+          offset: "-3:30",
+          description: "Newfoundland",
+        },
+      },
+      email: "alice.miller@example.com",
+      login: {
+        uuid: "0a95fd08-e3da-4125-8cd3-7180e3ce9435",
+        username: "brownladybug189",
+        password: "southpark",
+        salt: "m1QeiwqV",
+        md5: "200e65e423fe6be4748efc392da0407f",
+        sha1: "3a448dd19bf47484fb979edcbdcb162194bde8a9",
+        sha256:
+          "709e7c48d79552693c9af6ac22720d5d586d780c16afa107f2fb091cc54fc859",
+      },
+      dob: {
+        date: "1974-08-19T01:03:10.909Z",
+        age: 47,
+      },
+      registered: {
+        date: "2003-01-29T18:00:24.514Z",
+        age: 18,
+      },
+      phone: "179-521-3880",
+      cell: "045-401-2449",
+      id: {
+        name: "",
+        value: null,
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/90.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/90.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/90.jpg",
+      },
+      nat: "CA",
+    },
+  ]);
+
+  useEffect(() => {
+    API.getEmployees()
+      .then((res) => {
+        console.log(res.data.results);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
       <div className="row">
         <div className="col">
           <div className="text-center">
-            <table class="table">
+            <table className="table">
               <thead>
                 <tr>
                   <th scope="col">Image</th>
@@ -17,13 +87,19 @@ const Home = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>picture.thumbnail</td>
-                  <td>Name.first name.last</td>
-                  <td>Phone</td>
-                  <td>Email</td>
-                  <td>DoB.date</td>
-                </tr>
+                {employees.map((employee) => (
+                  <tr>
+                    <td>
+                      <img src={employee.picture.medium} alt="Employee Image" />
+                    </td>
+                    <td>
+                      {employee.name.first} {employee.name.last}
+                    </td>
+                    <td>{employee.phone}</td>
+                    <td>{employee.email}</td>
+                    <td>{moment(employee.dob.date).format("MM-DD-YYYY")}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
