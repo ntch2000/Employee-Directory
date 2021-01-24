6f3dabd18afc1825fc404af2b03ce96384a74d39
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2021-01-23 19:45:59 -0500

    Added search functionality. Created new searchEmployee state

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,31 +1,40 @@
 import React, { useEffect, useState } from "react";
 import API from "../utils/API";
-import EmployeeRow from "../components/EmployeeRow";
+import EmployeeRow from "../components/EmployeeRow/EmployeeRow/EmployeeRow";
 
 const Home = () => {
-  // set state
+  // employees state
   const [employees, setEmployees] = useState([]);
 
+  // filter order state
   const [sortOrder, setSortOrder] = useState("");
+
+  // search order state
+  const [searchEmployees, setSearchEmployees] = useState([]);
 
   // makes API call when page loads
   useEffect(() => {
     API.getEmployees()
       .then((res) => {
-        console.log(res.data.results);
+        //console.log(res.data.results);
         setEmployees(res.data.results);
+        setSearchEmployees(res.data.results);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  // sort function for ascending and descending order
   const sortBy = () => {
     let sortedEmployees = [];
 
+    // sorts the data in ascending order
     if (!sortOrder || sortOrder === "ascending") {
       sortedEmployees = [...employees].sort((a, b) =>
         a.name.first < b.name.first ? -1 : 1
       );
       setSortOrder("descending");
+
+      // sorts data in descending order
     } else {
       sortedEmployees = [...employees].sort((a, b) =>
         a.name.first > b.name.first ? -1 : 1
@@ -35,10 +44,46 @@ const Home = () => {
     console.log("test");
     console.log(sortedEmployees);
     setEmployees(sortedEmployees);
+    setSearchEmployees(sortedEmployees);
+  };
+
+  // search employees function
+  const search = (e) => {
+    const value = e.target.value;
+    console.log(`value: ${value}`);
+
+    if (value === "") {
+      setSearchEmployees(employees);
+      console.log(employees);
+    }
+
+    let filteredEmployees = [...employees].filter((employee) => {
+      return employee.name.first.toLowerCase().includes(value);
+    });
+
+    setSearchEmployees(filteredEmployees);
+    console.log("filtered");
+    console.log(filteredEmployees);
+    //console.log(searchEmployees);
+    console.log("employees");
+    console.log(employees);
   };
 
   return (
     <div>
+      <div className="row">
+        <div className="col-md-2 pt-3">
+          <div className="text-center">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              onChange={search}
+            />
+          </div>
+        </div>
+      </div>
       <div className="row">
         <div className="col">
           <div className="text-center">
@@ -56,7 +101,7 @@ const Home = () => {
               </thead>
               <tbody>
                 {/* maps over employees state and passes properties to EmployeeRow component */}
-                {employees.map((employee) => (
+                {searchEmployees.map((employee) => (
                   <EmployeeRow
                     key={employee.id.value}
                     first={employee.name.first}
